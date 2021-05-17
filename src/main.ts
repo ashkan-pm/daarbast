72a331e90893cc83dc40408944181bd7300e55d7
Ashkan Pourghasem <ashkan.pourghasem@googlemail.com>
2021-05-17 18:57:14 +0400

    Increase coverage

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import { TEST } from './constants';
 export function printTest() {
   console.log(TEST);
   console.log('Publish test');
-  setTimeout(() => {
-    console.warn('Another test');
-  });
 }
 
 printTest();
